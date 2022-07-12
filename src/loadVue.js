6921a93251c88c6a15b10639903266f141861328
sunxi1997 <2351405472@qq.com>
2022-07-12 16:28:12 +0800

    feat: index

--- src/loadVue.js
+++ src/loadVue.js
@@ -7,18 +7,25 @@ export default function iconfontToVue(iconfontJSSource, distDir) {
   const result = [...source.matchAll(jsReg)]
 
   result.forEach(({ groups }) => {
-    const {
-      name,
-      viewBox,
-      // path
-    } = groups
-
-    const vue = JSIconVueTemplate(groups)
-
+    const name = getCompName(groups.name)
+    const vue = JSIconVueTemplate({ ...groups, name })
     fs.writeFileSync(path.join(distDir, `${ name }.js`), vue)
   })
 
+  const index = JSIndexTemplate(result)
+  fs.writeFileSync(path.join(distDir, `index.js`), index)
 }
+
+const JSIndexTemplate = result => {
+  const nameList = result.map(({ groups }) => getCompName(groups.name))
+  let importContent = ''
+  nameList.forEach(name => {
+    importContent += `import ${ name } from './${ name }';\r`
+  })
+  let exportContent = `export {\r  ${ nameList.join(',\r  ') }\r}`
+  return `${ importContent }\r ${ exportContent }`
+}
+
 
 const JSIconVueTemplate = ({ name, path, viewBox }) => {
   const pathList = [...path.matchAll(jsPathReg)].map(r => r.groups.d)
@@ -37,7 +44,7 @@ const attrs = {
 const path = ${ pathStr }
 
 export default defineComponent({
-  name: '${ name }',
+  name: '${ getCompName(name) }',
   render() {
     openBlock()
     return createBlock('svg', attrs, path)
@@ -68,3 +75,8 @@ export default defineComponent({
   }
 });
 `
+
+
+function getCompName(name) {
+  return name.replaceAll(/(-|^)(.)/g, ($0, $1, $2) => $2.toUpperCase())
+}
