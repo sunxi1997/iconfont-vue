6f345f9f1c923a6f3fbd9cca48da3d2025047104
sunxi1997 <2351405472@qq.com>
2022-08-01 15:19:36 +0800

    feat: svg 转为 icon vue

--- src/svgToVue.js
+++ src/svgToVue.js
@@ -1,37 +1,44 @@
 import fs from "fs";
 import path from "path";
-import { jsPathReg, svgReg } from "./utils/reg.js";
+import { svgReg } from "./utils/reg.js";
 import { getCompName } from "./utils/index.js";
 
-export default function svgToVue(svgSourceDir, distDir) {
+export default function svgToVue(svgSourceDir, distDir, { fileNameCallback, CompNameCallback } = {}) {
   const fileList = fs.readdirSync(svgSourceDir)
   fileList.forEach(file => {
     const name = getCompName(file.replace(/(^.+)\.svg$/, (_, $1) => $1))
-    const svg = fs.readFileSync(path.join(svgSourceDir, file)).toString()
+    let svg = fs.readFileSync(path.join(svgSourceDir, file)).toString()
     const res = svg.match(svgReg)
     if (!res) {
       return
     }
     const { width, height } = res.groups
     const viewBox = `0 0 ${ width } ${ height }`
-    svg.replace(/(<svg[^>]+)\swidth="(\d+)"/, (_, $1) => $1)
-    svg.replace(/(<svg[^>]+)\sheight="(\d+)"/, (_, $1) => $1)
-    svg.replace(/<svg\s/, `<svg viewBox=${ viewBox }`)
+    svg = svg
+    .replace(/(<svg[^>]*)\swidth="(\d+)"/, (_, $1) => $1)
+    .replace(/(<svg[^>]*)\sheight="(\d+)"/, (_, $1) => $1)
+    .replace(/<svg\s/, `<svg viewBox="${ viewBox }" `)
 
-    const iconVue = JSIconVueTemplate(name, svg)
+    const iconVue = JSIconVueTemplate(name, svg, CompNameCallback)
 
-    fs.writeFileSync(path.join(distDir, `${ name }.vue`), iconVue)
+    const fileName = fileNameCallback ? fileNameCallback(name) : name
+
+    fs.writeFileSync(path.join(distDir, `${ fileName }.vue`), iconVue)
   })
 }
 
-function JSIconVueTemplate(name, content) {
+function JSIconVueTemplate(name, content, CompNameCallback) {
+  let compName = getCompName(name)
+  if (CompNameCallback) {
+    compName = CompNameCallback(compName)
+  }
   return `<template>
   ${ content }
 </template>
 
 <script>
 export default {
-  name: '${ getCompName(name) }'
+  name: '${ compName }'
 }
 </script>
 `
